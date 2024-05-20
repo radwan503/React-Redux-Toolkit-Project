e896dd276365c010c690fb1d8e245067d9652ef4
radwan503 <radwancse503@gmail.com>
2024-05-21 01:08:19 +0600

    react redux re learning

---
+++ src/features/counter/Counter.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment, decrement, reset, incrementByAmount } from './counterSlice'
+
+const Counter = () => {
+ const dispatch = useDispatch()
+ const [incrementAmount, setIncrementAmount] = useState(0)
+ const count = useSelector((state) => state.counter.count)
+
+ const addValue = Number(incrementAmount) || 0;
+
+ const resetAll = () => {
+  setIncrementAmount(0)
+  dispatch(reset())
+ }
+
+ return (
+  <section>
+   <p>{count}</p>
+   <div>
+
+    <button onClick={() => dispatch(increment())}>+</button>
+    <button onClick={() => dispatch(decrement())}>-</button>
+
+   </div>
+   <input value={incrementAmount} onChange={(e) => setIncrementAmount(e.target.value)} />
+   <div>
+    <button onClick={() => dispatch(incrementByAmount(addValue))}>Add Amount</button>
+    <button onClick={resetAll}>Reset</button>
+   </div>
+  </section>
+ )
+}
+
+export default Counter
